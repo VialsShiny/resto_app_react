fd252c751caba6889d24939f0adfd87e1b79c2ed
Drake <150128459+VialsShiny@users.noreply.github.com>
2025-05-25 20:00:18 +0200

    Fixing class on ErrorPages.jsx

--- frontend/src/pages/errorPage.jsx
+++ frontend/src/pages/errorPage.jsx
@@ -5,13 +5,13 @@ const ErrorPage = ({ status = 404 }) => {
     case 404:
       return (
         <>
-          <div class="w-full min-h-screen flex flex-col justify-center items-center px-12 lg:px-16 text-center">
-            <h2 class="flex gap-x-4 lg:gap-x-8 items-center text-8xl lg:text-[10rem] font-extrabold tracking-wide text-[#F54748]">4<img class="w-24 h-24 lg:w-36 lg:h-36" src="http://localhost:8000/storage/assets/logo/error_number.png" alt="0 mais avec une pizza" draggable="false" />4</h2>
-            <strong class="text-4xl">Whoops... Page Not Found</strong>
-            <p class="max-w-3xl font-medium text-gray-400 text-lg mt-6">
+          <div className="w-full min-h-screen flex flex-col justify-center items-center px-12 lg:px-16 text-center">
+            <h2 className="flex gap-x-4 lg:gap-x-8 items-center text-8xl lg:text-[10rem] font-extrabold tracking-wide text-[#F54748]">4<img className="w-24 h-24 lg:w-36 lg:h-36" src="http://localhost:8000/storage/assets/logo/error_number.png" alt="0 mais avec une pizza" draggable="false" />4</h2>
+            <strong className="text-4xl">Whoops... Page Not Found</strong>
+            <p className="max-w-3xl font-medium text-gray-400 text-lg mt-6">
               The page you are looking for might have been removed had its name changed or is temporarily unavailable
             </p>
-            <Link to="/" class="bg-[#F54748] text-white text-md font-bold tracking-wide rounded-full px-8 py-2 mt-12 hover:bg-red-600 transition duration-500">
+            <Link to="/" className="bg-[#F54748] text-white text-md font-bold tracking-wide rounded-full px-8 py-2 mt-12 hover:bg-red-600 transition duration-500">
               GO TO HOMEPAGE
             </Link>
           </div>
@@ -20,15 +20,15 @@ const ErrorPage = ({ status = 404 }) => {
     case 505:
       return (
         <>
-          <div class="w-full min-h-screen flex flex-col justify-center items-center px-12 lg:px-16 text-center">
-            <h2 class="flex gap-x-4 lg:gap-x-8 items-center text-8xl lg:text-[10rem] font-extrabold tracking-wide text-[#F54748]">
-              5<img class="w-24 h-24 lg:w-36 lg:h-36" src="http://localhost:8000/storage/assets/logo/error_number.png" alt="0 décoratif" draggable="false" />0
+          <div className="w-full min-h-screen flex flex-col justify-center items-center px-12 lg:px-16 text-center">
+            <h2 className="flex gap-x-4 lg:gap-x-8 items-center text-8xl lg:text-[10rem] font-extrabold tracking-wide text-[#F54748]">
+              5<img className="w-24 h-24 lg:w-36 lg:h-36" src="http://localhost:8000/storage/assets/logo/error_number.png" alt="0 décoratif" draggable="false" />0
             </h2>
-            <strong class="text-4xl">Oops... Something Went Wrong</strong>
-            <p class="max-w-3xl font-medium text-gray-400 text-lg mt-6">
+            <strong className="text-4xl">Oops... Something Went Wrong</strong>
+            <p className="max-w-3xl font-medium text-gray-400 text-lg mt-6">
               The server encountered an internal error or misconfiguration and was unable to complete your request.
             </p>
-            <Link href="/" class="bg-[#F54748] text-white text-md font-bold tracking-wide rounded-full px-8 py-2 mt-12 hover:bg-red-600 transition duration-500">
+            <Link href="/" className="bg-[#F54748] text-white text-md font-bold tracking-wide rounded-full px-8 py-2 mt-12 hover:bg-red-600 transition duration-500">
               GO TO HOMEPAGE
             </Link>
           </div>
@@ -38,13 +38,13 @@ const ErrorPage = ({ status = 404 }) => {
     default:
       return (
         <>
-          <div class="w-full min-h-screen flex flex-col justify-center items-center px-12 lg:px-16 text-center">
-            <h2 class="flex gap-x-4 lg:gap-x-8 items-center text-8xl lg:text-[10rem] font-extrabold tracking-wide text-[#F54748]">4<img class="w-24 h-24 lg:w-36 lg:h-36" src="http://localhost:8000/storage/assets/logo/error_number.png" alt="0 mais avec une pizza" draggable="false" />4</h2>
-            <strong class="text-4xl">Whoops... Page Not Found</strong>
-            <p class="max-w-3xl font-medium text-gray-400 text-lg mt-6">
+          <div className="w-full min-h-screen flex flex-col justify-center items-center px-12 lg:px-16 text-center">
+            <h2 className="flex gap-x-4 lg:gap-x-8 items-center text-8xl lg:text-[10rem] font-extrabold tracking-wide text-[#F54748]">4<img className="w-24 h-24 lg:w-36 lg:h-36" src="http://localhost:8000/storage/assets/logo/error_number.png" alt="0 mais avec une pizza" draggable="false" />4</h2>
+            <strong className="text-4xl">Whoops... Page Not Found</strong>
+            <p className="max-w-3xl font-medium text-gray-400 text-lg mt-6">
               The page you are looking for might have been removed had its name changed or is temporarily unavailable
             </p>
-            <Link href="/" class="bg-[#F54748] text-white text-md font-bold tracking-wide rounded-full px-8 py-2 mt-12 hover:bg-red-600 transition duration-500">
+            <Link href="/" className="bg-[#F54748] text-white text-md font-bold tracking-wide rounded-full px-8 py-2 mt-12 hover:bg-red-600 transition duration-500">
               GO TO HOMEPAGE
             </Link>
           </div>
